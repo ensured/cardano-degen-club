d4c494cc710731d854ca4c02742fb61a4053b08f
ensured <walnuts@gmail.com>
2023-11-18 20:28:51 -0800

    refactor

--- components/CardanoLinks.jsx
+++ components/CardanoLinks.jsx
@@ -66,59 +66,40 @@ const CardanoLinks = () => {
     setActiveTab(activeTab)
   }
 
+  const tabTriggers = [
+    "officialCardano",
+    "wallets",
+    "dexs",
+    "marketplaces",
+    "metaverse",
+    "chartsAnalytics",
+    "lendingBorrowing",
+    "yieldAggregators",
+    "stablecoins",
+    "privacy",
+    "syntheticProtocols",
+    "oracles",
+    "memecoins",
+    "other",
+  ]
+
+  const tabContents = tabTriggers.map((trigger, index) => (
+    <TabsContent key={index} value={String(index + 1)}>
+      {renderLinkContent(trigger)}
+    </TabsContent>
+  ))
+
   return (
     <div className="container mx-auto px-2 py-4 break-all">
-      <Tabs onChange={handleOnChange} defaultValue={activeTab}>
+      <Tabs defaultValue={activeTab} onChange={handleOnChange}>
         <TabsList>
-          <TabsTrigger value="1">official cardano links</TabsTrigger>
-          <TabsTrigger value="2">wallets</TabsTrigger>
-          <TabsTrigger value="3">dexs</TabsTrigger>
-          <TabsTrigger value="4">marketplaces</TabsTrigger>
-          <TabsTrigger value="6">charts/analytics</TabsTrigger>
-          <TabsTrigger value="5">metaverse</TabsTrigger>
-          <TabsTrigger value="7">lending/borrowing</TabsTrigger>
-          <TabsTrigger value="8">yield aggregators</TabsTrigger>
-          <TabsTrigger value="9">stable coins</TabsTrigger>
-          <TabsTrigger value="10">privacy</TabsTrigger>
-          <TabsTrigger value="11">synthetic protocols</TabsTrigger>
-
-          <TabsTrigger value="12">oracles</TabsTrigger>
-          <TabsTrigger value="13">meme coins</TabsTrigger>
-          <TabsTrigger value="14">other</TabsTrigger>
+          {tabTriggers.map((trigger, index) => (
+            <TabsTrigger key={index} value={String(index + 1)}>
+              {trigger}
+            </TabsTrigger>
+          ))}
         </TabsList>
-
-        {/* Tab content */}
-        <TabsContent value="1">
-          {renderLinkContent("officialCardano")}
-        </TabsContent>
-        <TabsContent value="2">{renderLinkContent("wallets")}</TabsContent>
-        <TabsContent value="3">{renderLinkContent("dexs")}</TabsContent>
-        <TabsContent value="4">{renderLinkContent("marketplaces")}</TabsContent>
-        <TabsContent value="6">
-          {renderLinkContent("chartsAnalytics")}
-        </TabsContent>
-        <TabsContent value="5">{renderLinkContent("metaverse")}</TabsContent>
-        <TabsContent value="7">
-          {renderLinkContent("lendingBorrowing")}
-        </TabsContent>
-        <TabsContent value="14">{renderLinkContent("other")}</TabsContent>
-        <TabsContent value="8">
-          {renderLinkContent("yieldAggregators")}
-        </TabsContent>
-
-        <TabsContent value="9">{renderLinkContent("stablecoins")}</TabsContent>
-
-        <TabsContent value="10">{renderLinkContent("privacy")}</TabsContent>
-
-        <TabsContent value="11">
-          {renderLinkContent("syntheticProtocols")}
-        </TabsContent>
-
-        <TabsContent value="12">{renderLinkContent("oracles")}</TabsContent>
-
-        <TabsContent value="13">{renderLinkContent("memecoins")}</TabsContent>
-
-        <TabsContent value="14">{renderLinkContent("other")}</TabsContent>
+        {tabContents}
       </Tabs>
     </div>
   )
