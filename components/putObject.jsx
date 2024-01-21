fda43cc19a4049f03a0fd7731589134fe44a3ba8
ensured <walnuts@gmail.com>
2024-01-20 17:51:40 -0800

    fix

--- components/putObject.jsx
+++ components/putObject.jsx
@@ -1,7 +1,3 @@
-
-
-
-
 function ProfileForm({ className }) {
   function formatDateShorthand(dateString) {
     const date = new Date(dateString)
@@ -16,24 +12,22 @@ function ProfileForm({ className }) {
     return number.toString().padStart(2, "0")
   }
 
+  const putObject = async (text) => {
+    try {
+      const date = new Date().toISOString()
+      const jsonData = JSON.stringify({ date, text })
+      const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: `feedback/${formatDateShorthand(date)}.json`,
+        Body: jsonData,
+      }
 
-
-const putObject = async (text) => {
-
-  try {
-    const date = new Date().toISOString()
-    const jsonData = JSON.stringify({ date, text })
-    const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: `feedback/${formatDateShorthand(date)}.json`,
-      Body: jsonData,
+      await s3Client.send(new PutObjectCommand(params))
+    } catch (error) {
+      console.log(error)
     }
-
-    await s3Client.send(new PutObjectCommand(params))
-  } catch (error) {
-    console.log(error)
+    return
   }
-  return
 }
 
 export default putObject
