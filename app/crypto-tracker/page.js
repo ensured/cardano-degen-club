55131f6111b60f0c60e4f09f23bed11acc0c5a4b
ensured <walnuts@gmail.com>
2024-03-02 18:50:49 -0800

    LFG

--- app/crypto-tracker/page.js
+++ app/crypto-tracker/page.js
@@ -1,10 +1,54 @@
-import TradingViewChart from "../../components/TradingViewChart"
-const page = () => {
-  return (
-    <div className="px-2 flex justify-center items-center">
-      <TradingViewChart />
-    </div>
-  )
-}
+"use client";
+import { useState } from "react";
+import TradingViewChart from "../../components/TradingViewChart";
 
-export default page
+const Crypto = () => {
+  const [adaAmount, setAdaAmount] = useState(1);
+
+  // Example conversion rates
+  const adaToBtcConversionRate = 0.00002; // Example conversion rate from ADA to BTC
+  const adaToSatsConversionRate = 1000000; // Example conversion rate from ADA to SATs
+  const adaToLovelacesConversionRate = 100000; // Example conversion rate from ADA to lovelaces
+
+  const convertToBTC = () => {
+    const btcAmount = adaAmount * adaToBtcConversionRate;
+    return btcAmount.toLocaleString(); // Format the result for easy reading
+  };
+
+  const convertToSats = () => {
+    const satsAmount = adaAmount * adaToSatsConversionRate;
+    return satsAmount.toLocaleString(); // Format the result for easy reading
+  };
+
+  const convertToLovelaces = () => {
+    const lovelacesAmount = adaAmount * adaToLovelacesConversionRate;
+    return lovelacesAmount.toLocaleString(); // Format the result for easy reading
+  };
+
+  return (
+    <div className="px-2 flex flex-col justify-center items-center">
+      <TradingViewChart />
+
+      <div>
+        <label>Enter ADA amount:</label>
+        <input
+          type="number"
+          value={adaAmount}
+          onChange={(e) => setAdaAmount(e.target.value)}
+        />
+
+        <p>
+          {adaAmount} ADA is equal to {convertToBTC()} BTC.
+        </p>
+        <p>
+          {adaAmount} ADA is equal to {convertToSats()} SATs.
+        </p>
+        <p>
+          {adaAmount} ADA is equal to {convertToLovelaces()} lovelaces.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Crypto;
