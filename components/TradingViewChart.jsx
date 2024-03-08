bbfea9ba9dccab5ea1dba5db33fe6a89609f6b8e
ensured <walnuts@gmail.com>
2024-03-07 19:12:41 -0800

    dark/light mode switch for tradingview chart

--- components/TradingViewChart.jsx
+++ components/TradingViewChart.jsx
@@ -1,60 +1,98 @@
 "use client"
 
 import { memo, useEffect, useRef } from "react"
+import { useTheme } from "next-themes"
 
 function TradingViewWidget() {
+  const { theme } = useTheme()
   const container = useRef()
 
   useEffect(() => {
-    const script = document.createElement("script")
-
-    script.src =
-      "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js"
-    script.type = "text/javascript"
-    script.async = true
-    script.innerHTML = `
-      {
-        "symbols": [
-          ["BINANCE:ADAUSD|7D"],
-          ["CRYPTOCAP:BTC.D|7D"],
-          ["COINBASE:ADABTC|7D"],
-          ["COINBASE:ADAETH|7D"]
-        ],
-        "chartOnly": false,
-        "width": "100%",
-        "height": "500",
-        "locale": "en",
-        "colorTheme": "dark",
-        "autosize": true,
-        "showVolume": false,
-        "showMA": false,
-        "hideDateRanges": false,
-        "hideMarketStatus": false,
-        "hideSymbolLogo": false,
-        "scalePosition": "right",
-        "scaleMode": "Normal",
-        "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-        "fontSize": "10",
-        "noTimeScale": true,
-        "valuesTracking": "1",
-        "changeMode": "price-and-percent",
-        "chartType": "area",
-        "maLineColor": "#2962FF",
-        "maLineWidth": 1,
-        "maLength": 9,
-        "lineWidth": 2,
-        "lineType": 0,
-        "dateRanges": [
-          "1d|1",
-          "1m|30",
-          "3m|60",
-          "12m|1D",
-          "60m|1W"
-        ]
+    const loadChart = () => {
+      // Check if the container is available
+      if (!container.current) {
+        return
       }
-    `
-    container.current.appendChild(script)
-  }, [])
+
+      // Remove the existing chart
+      container.current.innerHTML = ""
+
+      // Create a new script with the updated theme
+      const script = document.createElement("script")
+      script.src =
+        "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js"
+      script.type = "text/javascript"
+      script.async = true
+      script.innerHTML = `
+        {
+          "symbols": [
+            ["BINANCE:ADAUSD|7D"],
+            ["CRYPTOCAP:ADA.D|7D"],
+            ["CRYPTOCAP:BTC.D|7D"],
+            ["COINBASE:ADABTC|7D"],
+            ["COINBASE:ADAETH|7D"]
+          ],
+          "chartOnly": false,
+          "width": "100%",
+          "height": "500",
+          "locale": "en",
+          "colorTheme": "${theme}",
+          "autosize": true,
+          "showVolume": true,
+          "showMA": false,
+          "hideDateRanges": false,
+          "hideMarketStatus": false,
+          "hideSymbolLogo": false,
+          "scalePosition": "right",
+          "scaleMode": "Normal",
+          "fontFamily": "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
+          "fontSize": "${
+            window.innerWidth < 600
+              ? "12"
+              : window.innerWidth < 700
+              ? "14"
+              : window.innerWidth < 800
+              ? "18"
+              : "20"
+          }",
+          "noTimeScale": true,
+          "valuesTracking": "1",
+          "changeMode": "price-and-percent",
+          "chartType": "area",
+          "maLineColor": "#2962FF",
+          "maLineWidth": 1,
+          "maLength": 9,
+          "lineWidth": 2,
+          "lineType": 0,
+          "dateRanges": [
+            "1d|1",
+            "1m|30",
+            "3m|60",
+            "12m|1D",
+            "60m|1W",
+            "all|1M"
+          ]
+        }
+      `
+
+      // Append the new script to the container
+      container.current.appendChild(script)
+    }
+
+    // Load the chart when the component mounts
+    loadChart()
+    const handleResize = () => {
+      loadChart()
+    }
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize)
+
+    // Cleanup the event listener when the component unmounts
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [theme])
 
   return (
     <div className="tradingview-widget-container" ref={container}>
