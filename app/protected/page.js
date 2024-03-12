d381b6b1b2b97a927eb3f06d57131d7c9a5b0c57
ensured <walnuts@gmail.com>
2024-03-11 22:07:24 -0700

    gn

--- app/protected/page.js
+++ app/protected/page.js
@@ -1,3 +1,5 @@
+"use server";
+
 import {
   LoginLink,
   LogoutLink,
@@ -6,14 +8,14 @@ import {
 import { Title } from "@radix-ui/react-dialog"
 
 import { CardTitle } from "@/components/ui/card"
-
+import Comments from "@/components/Comments"
 import { GetObjectCommand, ListObjectsV2Command, s3Client } from "../../lib/s3"
 
-async function listFeedbackObjects() {
+export async function listFeedbackObjects() {
   try {
     const params = {
       Bucket: process.env.S3_BUCKET_NAME,
-      Prefix: "feedback/",
+      Prefix: "feedback/", getFeedbackDataByKey
     }
 
     const response = await s3Client.send(new ListObjectsV2Command(params))
@@ -26,7 +28,7 @@ async function listFeedbackObjects() {
   }
 }
 
-async function getFeedbackDataByKey(key) {
+export async function getFeedbackDataByKey(key) {
   try {
     const params = {
       Bucket: process.env.S3_BUCKET_NAME,
@@ -64,86 +66,103 @@ function extractTitle(key) {
   return title
 }
 
-const commentStyle = {
-  border: "1px solid #ccc",
-  borderRadius: "6px",
-  margin: "5px",
-  padding: "5px",
-}
+
 
 const page = async () => {
   const { getUser, isAuthenticated } = getKindeServerSession()
-  const user = await getUser()
+  const user = getUser()
   if (isAuthenticated) {
     if (user?.email === "finalemail417@gmail.com") {
-      const { success, feedbackObjects } = await listFeedbackObjects()
-      if (success) {
-        if (feedbackObjects) {
-          const feedbackList = await Promise.all(
-            feedbackObjects.map(async (object) => {
-              const { success: getSuccess, feedbackData } =
-                await getFeedbackDataByKey(object.Key)
-              return getSuccess ? feedbackData : null
-            })
-          )
-          return (
-            <div className="flex flex-col items-center justify-center">
-              <CardTitle>Comments</CardTitle>
-              <div className=" flex flex-wrap justify-center">
-                {/* Display feedback data here */}
 
-                {feedbackList.length > 0 &&
-                  feedbackList.map((feedback, index) => (
-                    <div key={index} style={commentStyle}>
-                      <p>
-                        <strong>Date:</strong> {feedback.date}
-                      </p>
-                      <p>
-                        <strong>Name: </strong>
-                        {extractTitle(feedbackObjects[index].Key)}
-                      </p>
-                      <p>
-                        <strong>Feedback: </strong> {feedback.feedback}
-                      </p>
-                    </div>
-                  ))}
-                <LogoutLink className="mx-20 flex w-full items-center justify-center rounded-md bg-teal-700 p-2 px-5 text-slate-50 dark:bg-teal-700 dark:text-slate-950">
-                  Log out
-                </LogoutLink>
-              </div>
-            </div>
-          )
-        } else {
-          return (
-            <div className="flex flex-col items-center justify-center pt-40">
-              {/* Display feedback data here */}
-              <CardTitle>No Comments</CardTitle>
-              <LogoutLink className="rounded-md bg-teal-900 p-2 px-5">
-                Log out
-              </LogoutLink>
-            </div>
-          )
-        }
-      }
+      return (
+        <>
+
+          <div className="flex flex-col items-center justify-center">
+            <Comments />
+          </div>
+
+        </>
+      )
+    } else {
+      return (
+        <div className="flex flex-col items-center justify-center pt-40">
+          {/* Display feedback data here */}
+          <CardTitle>No Comments</CardTitle>
+          <LogoutLink className="rounded-md bg-teal-900 p-2 px-5">
+            Log out
+          </LogoutLink>
+        </div>
+      )
     }
   }
-  return (
-    <div className="flex flex-col items-center justify-center pt-40">
-      {user ? (
-        <>
-          {user.given_name}, this page is restricted.
-          <LogoutLink>Log out</LogoutLink>
-        </>
-      ) : (
-        <>
-          <h2>Not authenticated</h2>
-          <LoginLink className="rounded-md bg-teal-900 p-2 px-5">
-            Log in
-          </LoginLink>
-        </>
-      )}
-    </div>
-  )
 }
+// const { success, feedbackObjects } = await listFeedbackObjects()
+// if (success) {
+//   if (feedbackObjects) {
+//     const feedbackList = await Promise.all(
+//       feedbackObjects.map(async (object) => {
+//         const { success: getSuccess, feedbackData } =
+//           await getFeedbackDataByKey(object.Key)
+//         return getSuccess ? feedbackData : null
+//       })
+//     )
+//     return (
+//       <div className="flex flex-col items-center justify-center">
+//         <CardTitle>Comments</CardTitle>
+//         <div className=" flex flex-wrap justify-center">
+//           {/* Display feedback data here */}
+
+//           {feedbackList.length > 0 &&
+//             feedbackList.map((feedback, index) => (
+//               <div key={index} style={commentStyle}>
+//                 <p>
+//                   <strong>Date:</strong> {feedback.date}
+//                 </p>
+//                 <p>
+//                   <strong>Name: </strong>
+//                   {extractTitle(feedbackObjects[index].Key)}
+//                 </p>
+//                 <p>
+//                   <strong>Feedback: </strong> {feedback.feedback}
+//                 </p>
+//               </div>
+//             ))}
+//           <LogoutLink className="mx-20 flex w-full items-center justify-center rounded-md bg-teal-700 p-2 px-5 text-slate-50 dark:bg-teal-700 dark:text-slate-950">
+//             Log out
+//           </LogoutLink>
+//         </div>
+//       </div>
+//     )
+//   } else {
+//     return (
+//       <div className="flex flex-col items-center justify-center pt-40">
+//         {/* Display feedback data here */}
+//         <CardTitle>No Comments</CardTitle>
+//         <LogoutLink className="rounded-md bg-teal-900 p-2 px-5">
+//           Log out
+//         </LogoutLink>
+//       </div>
+//     )
+//   }
+
+
+// return (
+//   <div className="flex flex-col items-center justify-center pt-40">
+//     {user ? (
+//       <>
+//         {user.given_name}, this page is restricted.
+//         <LogoutLink>Log out</LogoutLink>
+//       </>
+//     ) : (
+//       <>
+//         <h2>Not authenticated</h2>
+//         <LoginLink className="rounded-md bg-teal-900 p-2 px-5">
+//           Log in
+//         </LoginLink>
+//       </>
+//     )}
+//   </div>
+// )
+// }
 
 export default page
---
+++ components/Comments.jsx
@@ -0,0 +1,88 @@
+"use client"
+
+import { useEffect, useState } from "react"
+
+import { getFeedbackDataByKey, listFeedbackObjects } from "@/app/protected/page"
+
+import extractNameFromFeedbackString from "../lib/helper"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card"
+
+const Comments = () => {
+  const [comments, setComments] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    let isMounted = true
+
+    const fetchComments = async () => {
+      try {
+        const { success, feedbackObjects } = await listFeedbackObjects()
+        if (success && isMounted) {
+          if (feedbackObjects) {
+            const feedbackList = await Promise.all(
+              feedbackObjects.map(async (object) => {
+                const { success: getSuccess, feedbackData } =
+                  await getFeedbackDataByKey(object.Key)
+                const objKey = object.Key
+                return getSuccess ? { feedbackData, objKey } : null
+              })
+            )
+            isMounted && setComments(feedbackList)
+            isMounted && setLoading(false)
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching comments:", error)
+      }
+    }
+
+    fetchComments()
+
+    // Cleanup function
+    return () => {
+      isMounted = false
+    }
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center justify-center pt-16">
+      {loading ? (
+        <div className="h-5 w-5 animate-spin rounded-full border-t-4 border-dotted border-slate-50"></div>
+      ) : comments.length > 0 ? (
+        <Card className="">
+          <CardHeader>
+            <CardTitle>Comments</CardTitle>
+          </CardHeader>
+          <CardContent>
+            {comments.map((feedback, index) => (
+              <Card
+                key={index}
+                className="flex flex-col items-center flex-wrap"
+              >
+                <CardContent>
+                  <div className="text-lg font-bold bg-slate-700 opacity-50 px-4 py-2 mt-4 rounded-lg text-center">
+                    {extractNameFromFeedbackString(feedback.objKey)}
+                  </div>
+                  <div className="text-lg font-bold  px-4 pt-4 rounded-lg">
+                    Date: {feedback.feedbackData.date}
+                  </div>
+                  <div className="text-lg font-bold  px-4 rounded-lg">
+                    Comment: {feedback.feedbackData.feedback}
+                  </div>
+                </CardContent>
+              </Card>
+            ))}
+          </CardContent>
+        </Card>
+      ) : (
+        <div className="flex flex-col items-center justify-center">
+          <Card>
+            <CardTitle>No Comments</CardTitle>
+          </Card>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Comments
