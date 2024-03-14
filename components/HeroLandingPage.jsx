3c5bd93464c0bbc2c977cc7c6eb239ceb71d07b4
ensured <74529637+ensured@users.noreply.github.com>
2024-03-13 18:38:15 -0700

    add padding

--- components/HeroLandingPage.jsx
+++ components/HeroLandingPage.jsx
@@ -11,10 +11,10 @@ import { Input } from "@/components/ui/input"
 
 export default function HeroLandingPage() {
   return (
-    <section className="w-full bg-gray-100 pb-12 pt-10 dark:bg-gray-800 md:pb-24 lg:pb-32 xl:pb-48">
+    <section className="w-full bg-gray-100 pb-12 pt-14 dark:bg-gray-800 md:pb-24 lg:pb-32 xl:pb-48">
       <div className="container mx-auto px-4 md:px-6">
         <div className="flex flex-col items-center space-y-4 text-center">
-          <div className="space-y-2">
+          <div className="space-y-">
             <h1 className="text-3xl font-bold tracking-tight  text-zinc-900 dark:text-slate-50 sm:text-4xl md:text-5xl lg:text-6xl">
               Discover Cardano
             </h1>
