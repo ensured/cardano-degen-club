401bfcdb7db0503bf311f62f49403309c1e32219
ensured <74529637+ensured@users.noreply.github.com>
2024-03-15 16:23:25 -0700

    replace &#39; with &apos;

--- components/FeedbackClient.jsx
+++ components/FeedbackClient.jsx
@@ -89,7 +89,7 @@ export default function FeedBackDrawer() {
           <DialogHeader>
             <DialogTitle>Leave feedback</DialogTitle>
             <DialogDescription>
-              If there&#39;s anything you want me to add, modify, change let me
+              If there&apos;s anything you want me to add, modify, change let me
               know! Your feedback is important to me.
             </DialogDescription>
           </DialogHeader>
