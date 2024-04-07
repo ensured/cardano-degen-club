f44680dc842cdb1c422b90fb2185804be6138c77
ensured <74529637+ensured@users.noreply.github.com>
2024-04-07 12:31:24 -0700

    add stopPropagation to handleStarIconClick to prevent the FullTitleTooltip from being triggered

--- components/useRecipeSearch.js
+++ components/useRecipeSearch.js
@@ -202,6 +202,7 @@ const useRecipeSearch = () => {
 
   const handleStarIconClick = (index) => (e) => {
     e.preventDefault()
+    e.stopPropagation()
 
     const recipe = searchResults.hits[index].recipe
     const recipeName = extractRecipeName(recipe.shareAs)
