8e46afa05e0a0602af861fefdb3e9689fb83b524
ensured <74529637+ensured@users.noreply.github.com>
2024-04-08 12:44:25 -0700

    lol

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -28,7 +28,7 @@ export async function GET(request: NextRequest) {
   // Check if the request is within the rate limit window
   const lastSubmission = MemoryCache.get(cacheKey)
   if (lastSubmission && now - lastSubmission < RECIPIES_FETCH_TIMEOUT_MS) {
-    return Response.json({
+    return NextResponse.json({
       success: false,
       message: `Rate limit exceeded, please try again in ${Math.ceil(
         (RECIPIES_FETCH_TIMEOUT_MS - (now - lastSubmission)) / 1000
@@ -40,18 +40,18 @@ export async function GET(request: NextRequest) {
     const url = `https://api.edamam.com/api/recipes/v2?q=${input}&type=public&app_id=${process.env.APP_ID}&app_key=${process.env.APP_KEY}`
     const response = await fetch(url)
     if (response.status === 429) {
-      return Response.json({
+      return NextResponse.json({
         success: false,
         message: `API Usage limits are exceeded, try again later`,
       })
     }
     const data = await response.json()
-    return Response.json({
+    return NextResponse.json({
       success: true,
       data,
     })
   } catch (err) {
-    return Response.json({
+    return NextResponse.json({
       success: false,
       message: "An error occurred. Please try again later.",
     })
