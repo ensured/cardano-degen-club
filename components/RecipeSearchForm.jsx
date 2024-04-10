505787199d6e6899d265d5b4d92986eb8ea2a670
ensured <walnuts@gmail.com>
2024-04-09 22:16:11 -0700

    remove unused code

--- components/RecipeSearchForm.jsx
+++ components/RecipeSearchForm.jsx
@@ -9,10 +9,6 @@ import { Separator } from "@/components/ui/separator"
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 
-const tags = Array.from({ length: 50 }).map(
-  (_, i, a) => `v1.2.0-beta.${a.length - i}`
-)
-
 const RecipeSearchForm = ({
   searchRecipes,
   handleInputChange,
