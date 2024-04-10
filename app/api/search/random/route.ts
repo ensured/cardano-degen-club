bb8987aaadab4a524734da4b1cbcddc3bd0cf6a5
ensured <walnuts@gmail.com>
2024-04-09 21:14:27 -0700

    remove useless async function and force dynamic for route

--- app/api/search/random/route.ts
+++ app/api/search/random/route.ts
@@ -1,14 +1,11 @@
 import { NextRequest, NextResponse } from "next/server"
 import { foodItems } from "lib/foods"
 
-const getRandomFoodItem = async () => {
-  const index = Math.floor(Math.random() * foodItems.length)
-  return foodItems[index]
-}
-
 export const dynamic = "force-dynamic"
+
 export async function GET(req: NextRequest, res: NextResponse) {
-  const randomFood = await getRandomFoodItem()
+  const index = Math.floor(Math.random() * foodItems.length)
+  const randomFood = foodItems[index]
   const response = await fetch(
     `https://api.edamam.com/api/food-database/v2/parser?ingr=${randomFood}&app_id=${process.env.FOOD_API_APP_ID}&app_key=${process.env.FOOD_API_APP_KEY}`
   )
