67e0bd98463ecc324c58583da74a798338a6b455
ensured <74529637+ensured@users.noreply.github.com>
2024-04-14 17:13:20 -0700

    make layout async

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ interface RootLayoutProps {
   children: React.ReactNode
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+export default async function RootLayout({ children }: RootLayoutProps) {
   return (
     <>
       <html lang="en" suppressHydrationWarning>
