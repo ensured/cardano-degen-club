adc51ecb1ad7c236d147eccb9c229d6c2f077517
ensured <walnuts@gmail.com>
2024-04-15 19:27:05 -0700

    move button up 55px if near bottom

--- components/ScrollToTopButton.tsx
+++ components/ScrollToTopButton.tsx
@@ -5,22 +5,28 @@ import { ChevronUp } from "lucide-react"
 
 const ScrollToTopButton = () => {
   const [isVisible, setIsVisible] = useState(false)
+  const [isNearBottom, setIsNearBottom] = useState(false)
 
   useEffect(() => {
     const toggleVisibility = () => {
-      // if the user scrolls down, show the button
-      window.scrollY > 500 ? setIsVisible(true) : setIsVisible(false)
+      const scrolled = window.scrollY
+      const windowHeight = window.innerHeight
+      const documentHeight = document.documentElement.scrollHeight
+      const scrollableDistance = documentHeight - windowHeight
+      setIsVisible(scrolled > 500) // Show button when scrolled down
+      setIsNearBottom(scrolled > scrollableDistance - 100) // Check if near bottom
     }
-    // listen for scroll events
+
+    // Listen for scroll events
     window.addEventListener("scroll", toggleVisibility)
 
-    // clear the listener on component unmount
+    // Clear the listener on component unmount
     return () => {
       window.removeEventListener("scroll", toggleVisibility)
     }
   }, [])
 
-  // handles the animation when scrolling to the top
+  // Handles the animation when scrolling to the top
   const scrollToTop = () => {
     isVisible &&
       window.scrollTo({
@@ -33,6 +39,8 @@ const ScrollToTopButton = () => {
     <button
       className={`duration-250 fixed bottom-4 right-4 z-50 rounded-full p-2 outline-none transition-opacity ${
         isVisible ? "opacity-100" : "opacity-0"
+      } ${
+        isNearBottom ? "translate-y-[-55px]" : ""
       } bg-zinc-950 text-white shadow-[inset_0px_0px_1px_1px_#553C9A] hover:bg-zinc-900 hover:text-white focus:outline-none`}
       onClick={scrollToTop}
     >
