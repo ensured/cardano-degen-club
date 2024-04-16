78e798c0a6a9c54792a57d257bc5fb19fd103eeb
ensured <walnuts@gmail.com>
2024-04-15 18:21:39 -0700

    move contact info to the footer.

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,48 @@
+import Link from "next/link"
+
+import { siteConfig } from "@/config/site"
+
 import FeedBackDrawer from "./Feedback"
+import { Icons } from "./icons"
+import { buttonVariants } from "./ui/button"
 
 const Footer = () => {
   return (
     <footer className="bg-sky-200/70 shadow dark:bg-zinc-900">
-      <div className="flex w-full items-center justify-between gap-4 p-3">
-        <span className="text-sm text-gray-500 dark:text-gray-400">
-          <div className=" flex flex-col items-center justify-center gap-x-1 sm:flex-row">
-            <div>© 2024</div>{" "}
-            <div className=" text-sm text-gray-500 hover:underline dark:text-gray-400">
-              cardanodegen.club
-            </div>
-          </div>
-        </span>
+      <div className="flex w-full items-center justify-center gap-10 p-3 md:gap-12 ">
+        {/* <div className=" text-sm text-gray-500 hover:underline dark:text-gray-400">
+          © 2024 - cardanodegen.shop
+        </div> */}
         <FeedBackDrawer />
+
+        <div className="flex overflow-auto">
+          <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
+            <div
+              className={buttonVariants({
+                size: "icon",
+                variant: "ghost",
+              })}
+            >
+              <Icons.gitHub className="h-5 w-5" />
+              <span className="sr-only">GitHub</span>
+            </div>
+          </Link>
+          <Link
+            href={siteConfig.links.twitter}
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div
+              className={buttonVariants({
+                size: "icon",
+                variant: "ghost",
+              })}
+            >
+              <Icons.twitter className="h-5 w-5 fill-current" />
+              <span className="sr-only">Twitter</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </footer>
   )
