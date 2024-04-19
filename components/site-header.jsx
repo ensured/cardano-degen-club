da9d9c907e72218764730ebe3c19b5cdee86b8d2
ensured <74529637+ensured@users.noreply.github.com>
2024-04-19 14:30:04 -0700

    fix style

--- components/site-header.jsx
+++ components/site-header.jsx
@@ -18,7 +18,7 @@ import UserButton from "./UserButton"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full overflow-x-auto border-b bg-background">
-      <div className="m-1 flex h-16 w-full items-center gap-3 px-4 md:px-4">
+      <div className="m-1 flex h-16 w-full items-center gap-3 px-4 md:px-3">
         <MainNav />
         <Dropdown />
         <div className="flex flex-1 items-center justify-end space-x-4">
