6329e42860d552c04728e9aa244d06a699e4d230
ensured <74529637+ensured@users.noreply.github.com>
2024-06-21 15:10:42 -0700

    alternate every 4 videos until 20 videos are found

--- app/pickleball/page.js
+++ app/pickleball/page.js
@@ -19,13 +19,14 @@ export default function Home() {
 
   return (
     <div className="container mx-auto py-8">
-      <h1 className="text-3xl font-bold mb-6 text-center">
+      <h1 className="mb-6 text-center text-3xl font-bold">
         Pickleball Pro Doubles Matches
       </h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {videos.map((video) => (
-          <VideoCard key={video.id.videoId} video={video} />
-        ))}
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        {videos &&
+          videos.map((video) => (
+            <VideoCard key={video.id.videoId} video={video} />
+          ))}
       </div>
     </div>
   )
