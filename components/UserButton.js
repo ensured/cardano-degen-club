42d6935fab6980a1547a2b77b66a1a3f59c48dc4
ensured <74529637+ensured@users.noreply.github.com>
2024-10-18 00:27:17 -0700

    revert

--- components/UserButton.js
+++ components/UserButton.js
@@ -1,14 +1,19 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server"
+import { cn } from "@/lib/utils"
 
 import { LoginPopup, LogoutPopup } from "./LoginPopup"
+import { Button, buttonVariants } from "./ui/button"
 
 const UserButton = async () => {
   const { getUser } = getKindeServerSession()
-  const user = await getUser()
+  const user = await getUser().then((user) => user)
+
   if (!user) {
     return (
       <div className="group relative z-50 flex shrink-0 flex-col-reverse items-center justify-center">
-        <LogoutPopup />
+
+                  <Button variant={"ghost"}>Login</Bu
+
       </div>
     )
   }
