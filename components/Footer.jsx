580ac6ec6cd460c7b76bb253dc6d23b178f368e7
ensured <74529637+ensured@users.noreply.github.com>
2024-10-21 19:30:19 -0700

    revert

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,8 +7,11 @@ import { buttonVariants } from "./ui/button"
 
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 right-0 bg-white/85 shadow dark:bg-zinc-900/85">
-      <div className=" mx-auto flex w-full flex-wrap items-center justify-evenly gap-2 p-1 md:container">
+    <footer className="bg-white shadow dark:bg-zinc-900">
+      <div className="mx-auto flex w-full flex-wrap items-center justify-evenly gap-1 p-2 md:container">
+        <div className=" text-center text-xs font-medium text-zinc-800 hover:underline dark:text-gray-400 sm:text-sm">
+          © 2024 cardanodegen.shop
+        </div>
         <div className="flex overflow-auto">
           <Link href={siteConfig.links.github} target="_blank" rel="noreferrer">
             <div
