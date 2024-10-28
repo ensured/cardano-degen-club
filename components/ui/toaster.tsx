ebd62d53afb9baaee9c506238d944117f68a428f
ensured <74529637+ensured@users.noreply.github.com>
2024-10-28 15:04:00 -0700

    gg

--- components/ui/toaster.tsx
+++ components/ui/toaster.tsx
@@ -10,7 +10,6 @@ import { useToast } from "@/components/ui/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
-
   return (
     <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
