3e4bb740376883d839573e75cf3bce1f1fc7d33b
ensured <74529637+ensured@users.noreply.github.com>
2024-10-29 13:00:58 -0700

    fix

--- components/RecipeCard.jsx
+++ components/RecipeCard.jsx
@@ -60,25 +60,25 @@ const RecipeCard = ({ recipe, index, lastFoodItemRef, searchResults }) => (
     <Link
       target="_blank"
       href={recipe.recipe.shareAs}
-      className="block size-full"
+      className="flex size-full flex-col justify-center"
       aria-label={`View recipe for ${recipe.recipe.label}`}
     >
       <FullTitleToolTip title={recipe.recipe.label} className="block size-full">
-        <Card className="group flex size-full flex-col border-none transition-colors">
-          <CardHeader className="space-y-0 p-1.5 pb-0">
-            <div className="flex min-h-[1.8rem] items-center justify-center">
-              <h3 className="line-clamp-2 text-center text-xs font-medium leading-tight text-muted-foreground transition-colors group-hover:text-foreground lg:text-sm">
+        <Card className="group flex size-full flex-col items-center justify-between border-none p-1.5 transition-colors sm:p-2">
+          <CardHeader className="w-full space-y-0 p-0">
+            <div className="flex min-h-6 items-center justify-center sm:min-h-[1.8rem]">
+              <h3 className="line-clamp-2 text-center text-[0.7rem] font-medium leading-tight text-muted-foreground transition-colors group-hover:text-foreground sm:text-xs lg:text-sm">
                 {recipe.recipe.label}
               </h3>
             </div>
           </CardHeader>
-          <CardContent className="mt-auto flex flex-[2] items-center justify-center p-0.5">
+          <CardContent className="flex w-full items-center justify-center p-0">
             <Image
               src={recipe.recipe.images.SMALL.url}
               alt={recipe.recipe.label}
               width={80}
               height={80}
-              className="w-[51%] rounded-sm object-contain transition-transform duration-200 group-hover:scale-110 sm:w-3/5 md:w-[58%]"
+              className="w-3/5 rounded-sm object-contain transition-transform duration-200 group-hover:scale-105 "
               unoptimized
               priority
             />
@@ -106,10 +106,10 @@ export const RecipeCards = ({
   }))
 
   return (
-    <div className="mt-2 grid w-full grid-cols-3 gap-1 rounded-md sm:grid-cols-4 sm:gap-2 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8">
+    <div className="mt-2 grid w-full grid-cols-2 gap-1 rounded-md sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8">
       {recipesWithFavorites.map((recipe, index) => (
         <div
-          className="relative aspect-[4/3.5] w-full"
+          className="relative aspect-[4/3] w-full sm:aspect-[4/3.5]"
           key={recipe.recipe.shareAs}
         >
           <RecipeCard
