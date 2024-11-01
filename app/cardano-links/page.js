40490f91fc44bd0df2159c6f8588f32dc2767acf
ensured <74529637+ensured@users.noreply.github.com>
2024-11-01 13:26:44 -0700

    gg

--- app/cardano-links/page.js
+++ app/cardano-links/page.js
@@ -13,9 +13,9 @@ export const metadata = {
 const CardanoLinksPage = () => {
   return (
     <Animation>
-      <div className="mx-auto h-full max-w-7xl px-6 py-4">
-        <div className="mb-6 space-y-2 text-center">
-          <h1 className="bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text text-4xl font-extrabold tracking-tight text-transparent sm:text-5xl animate-gradient-x dark:from-blue-300 dark:via-purple-300 dark:to-pink-300">
+      <div className="mx-auto h-full max-w-7xl px-1 py-4 sm:px-10">
+        <div className="mb-6 space-y-2 text-center p-4">
+          <h1 className="bg-gradient-to-r from-primary via-purple-500 to-pink-500 bg-clip-text text-3xl font-extrabold tracking-tight text-transparent sm:text-5xl animate-gradient-x dark:from-blue-300 dark:via-purple-300 dark:to-pink-300">
             Cardano Resources Directory
           </h1>
           <p className="mx-auto max-w-2xl text-base text-muted-foreground sm:text-lg ">
