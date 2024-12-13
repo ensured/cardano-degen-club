2f2c393c32f3edf62096ff43d904f92a65c666f5
ensured <74529637+ensured@users.noreply.github.com>
2024-12-12 17:07:56 -0800

    gg

--- app/clerk/Provider.jsx
+++ app/clerk/Provider.jsx
@@ -3,7 +3,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 
 export default function CustomClerkProvider({children}) {
   return (
-    <ClerkProvider publishableKey={"pk_live_Y2xlcmsuY2FyZGFub2RlZ2VuLnNob3Ak"}>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       {children}
     </ClerkProvider>
   );
