e41ec860e90184eb3b6586c1a3d757f01c4860fe
ensured <74529637+ensured@users.noreply.github.com>
2024-12-18 15:05:16 -0800

    update revalidate: 60 from revalidate: 0

--- app/api/last-commits/route.ts
+++ app/api/last-commits/route.ts
@@ -10,7 +10,7 @@ const fetchJson = async (url: string) => {
   const response = await fetch(url, {
     headers: GITHUB_HEADERS,
     next: {
-      revalidate: 0,
+      revalidate: 60,
     },
   })
   if (!response.ok) {
