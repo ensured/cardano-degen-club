26df72659185b92670d44b00233c23fa1698826d
ensured <74529637+ensured@users.noreply.github.com>
2024-12-18 23:17:06 -0800

    gg

--- app/api/ada-price/route.ts
+++ app/api/ada-price/route.ts
@@ -3,55 +3,35 @@ import { NextRequest } from "next/server"
 
 export const runtime = "edge"
 
-const CACHE_DURATION = 10000 // 10 seconds
-type CachedData = {
-  [key: string]: {
-    usd: number
-  }
-}
-
-let cachedData: CachedData | null = null
-let lastFetchTime = 0
-
 export async function GET(request: NextRequest) {
   const currentTime = Date.now()
-
-  // Check if cached data is still valid
-  if (cachedData && currentTime - lastFetchTime < CACHE_DURATION) {
-    return NextResponse.json(cachedData)
-  }
 
   try {
     const response = await fetch(
       process.env.COIN_GECKO_API_URL ||
-        "https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd"
+        "https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd",
+      {
+        next: {
+          revalidate: 30,
+        },
+      }
     )
 
     if (!response.ok) {
       throw new Error(`Error fetching data: ${response.statusText}`)
     }
 
-    const data: CachedData = await response.json()
-
-    // Update cache
-    cachedData = data
-    lastFetchTime = currentTime
-
-    // Set a cookie with the fetched price
-    const cookie = `adaPrice=${JSON.stringify(data)}; Max-Age=10; Path=/; HttpOnly`
+    const data = await response.json()
     const res = NextResponse.json(data)
-    res.headers.append("Set-Cookie", cookie)
 
     // Add cache control headers
     res.headers.append(
       "Cache-Control",
-      "s-maxage=10, stale-while-revalidate=59"
+      "s-maxage=30, stale-while-revalidate=29"
     )
 
     // Add CORS headers
     res.headers.append("Access-Control-Allow-Origin", "*") // Allow all origins
-    res.headers.append("Access-Control-Allow-Methods", "GET, OPTIONS") // Allow specific methods
-    res.headers.append("Access-Control-Allow-Headers", "Content-Type") // Allow specific headers
 
     return res
   } catch (error) {
