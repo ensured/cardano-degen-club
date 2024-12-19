0bf08946ef973dab0384b89235d03a4c1ff24205
ensured <74529637+ensured@users.noreply.github.com>
2024-12-19 15:04:52 -0800

    createRouteMatcher in middleware

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,13 @@
-import { clerkMiddleware } from "@clerk/nextjs/server"
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server"
 
-export default clerkMiddleware()
+const isProtectedRoute = createRouteMatcher([
+  "/recipe-fren(.*)",
+  "/crypto-tracker(.*)",
+])
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect()
+})
 
 export const config = {
   matcher: [
