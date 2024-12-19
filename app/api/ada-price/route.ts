bf01f08d58fbb415ab15a850ff27d3bdbe1e7360
ensured <74529637+ensured@users.noreply.github.com>
2024-12-18 23:00:45 -0800

    test1

--- app/api/ada-price/route.ts
+++ app/api/ada-price/route.ts
@@ -3,23 +3,46 @@ import { NextRequest } from "next/server"
 
 export const runtime = "edge"
 
+const CACHE_DURATION = 10000 // 10 seconds
+type CachedData = {
+  [key: string]: {
+    usd: number
+  }
+}
+
+let cachedData: CachedData | null = null
+let lastFetchTime = 0
+
 export async function GET(request: NextRequest) {
+  const currentTime = Date.now()
+
+  // Check if cached data is still valid
+  if (cachedData && currentTime - lastFetchTime < CACHE_DURATION) {
+    return NextResponse.json(cachedData)
+  }
+
   try {
     const response = await fetch(
-      "https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd",
-      {
-        next: {
-          revalidate: 60,
-        },
-      }
+      process.env.COIN_GECKO_API_URL ||
+        "https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=usd"
     )
 
     if (!response.ok) {
       throw new Error(`Error fetching data: ${response.statusText}`)
     }
 
-    const data = await response.json()
-    return NextResponse.json(data)
+    const data: CachedData = await response.json()
+
+    // Update cache
+    cachedData = data
+    lastFetchTime = currentTime
+
+    // Set a cookie with the fetched price
+    const cookie = `adaPrice=${JSON.stringify(data)}; Max-Age=10; Path=/; HttpOnly`
+    const res = NextResponse.json(data)
+    res.headers.append("Set-Cookie", cookie)
+
+    return res
   } catch (error) {
     return NextResponse.json(
       { error: (error as Error)?.message },
