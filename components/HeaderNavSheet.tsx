1154ccea00108ffee069fe351e6968fafd5aca6f
ensured <74529637+ensured@users.noreply.github.com>
2024-12-24 14:37:28 -0800

    make svg width a bit bigger

--- components/HeaderNavSheet.tsx
+++ components/HeaderNavSheet.tsx
@@ -124,7 +124,7 @@ export function HeaderNavSheet() {
                   <span className="sr-only">ADA Handle</span>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    width="61"
+                    width="62"
                     height="20"
                     viewBox="0 7 120 41"
                   >
