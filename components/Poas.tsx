16b61c6a229170e3b7c280b02d40ca5724674425
ensured <74529637+ensured@users.noreply.github.com>
2025-01-19 12:44:04 -0800

    update styles

--- components/Poas.tsx
+++ components/Poas.tsx
@@ -1530,7 +1530,7 @@ export default function Poas() {
               </Button>
             </div>
           </DialogHeader>
-          <div className="grid grid-cols-2 gap-4 p-4 md:grid-cols-3">
+          <div className="grid grid-cols-1 gap-1 p-2 md:grid-cols-2 lg:grid-cols-3">
             {pinataResponse.rows.map((file) => (
               <div
                 key={file.ipfs_pin_hash}
