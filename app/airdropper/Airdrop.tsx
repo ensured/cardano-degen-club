da9f85c567a4bc94cc0caa4732d34bc7d337dda8
ensured <74529637+ensured@users.noreply.github.com>
2025-01-29 18:58:40 -0800

    Update Airdrop.tsx

--- app/airdropper/Airdrop.tsx
+++ app/airdropper/Airdrop.tsx
@@ -540,7 +540,7 @@ const Airdrop = () => {
                         </div>
                       ) : (
                         <div className="flex items-center gap-1.5">
-                          <span>Select Token to Airdrop</span>
+                          <span>Select Token</span>
                           <div className="flex items-center justify-center gap-1">
                             <span className="mt-0.5 text-sm text-muted-foreground">
                               ({Object.keys(assetDetails).length})
