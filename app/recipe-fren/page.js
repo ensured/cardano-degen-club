6b6cd2b4dc1fa13b509208a5c4089bb84a8f5106
ensured <74529637+ensured@users.noreply.github.com>
2025-02-09 14:06:26 -0800

    fix?

--- app/recipe-fren/page.js
+++ app/recipe-fren/page.js
@@ -20,51 +20,51 @@ import UserLoginButtons from '@/components/UserLoginButtons'
 // }
 
 const TOAST_OPTIONS = {
-	className: 'dark:bg-zinc-950 dark:text-slate-100',
-	position: 'bottom-center',
-	success: {
-		style: {
-			background: 'green',
-		},
-	},
-	error: {
-		style: {
-			background: 'red',
-		},
-	},
+  className: 'dark:bg-zinc-950 dark:text-slate-100',
+  position: 'bottom-center',
+  success: {
+    style: {
+      background: 'green',
+    },
+  },
+  error: {
+    style: {
+      background: 'red',
+    },
+  },
 }
 
 const RecipeFrenPage = () => {
-	const { user } = useUser()
-	const { walletState } = useWallet()
-	const userEmail = user?.emailAddresses[0]?.emailAddress
+  const { user } = useUser()
+  const { walletState } = useWallet()
+  const userEmail = user?.emailAddresses[0]?.emailAddress
 
-	if (!userEmail && !walletState.stakeAddress) {
-		return (
-			<Animation>
-				<div className="flex w-full justify-center pt-6 text-center">
-					<div className="flex flex-col items-center justify-center gap-2 p-2 text-xl sm:text-2xl">
-						👋 Welcome to Recipe Fren!
-						<div className="max-w-[22rem] p-4 text-sm dark:text-gray-400/60 md:max-w-[26rem]">
-							Sign in to unlock all the delicious recipes and the ability to save your favorite recipes and even
-							download them as a formatted PDF file!
-						</div>
-						<div className="flex flex-row items-center justify-center rounded-lg border border-border text-xl sm:text-2xl">
-							<UserLoginButtons extraText="Sign in" />
-						</div>
-					</div>
-				</div>
-			</Animation>
-		)
-	}
+  if (!userEmail && !walletState.stakeAddress) {
+    return (
+      <Animation>
+        <div className="flex w-full justify-center pt-6 text-center">
+          <div className="flex flex-col items-center justify-center gap-2 p-2 text-xl sm:text-2xl">
+            👋 Welcome to Recipe Fren!
+            <div className="max-w-[22rem] p-4 text-sm dark:text-gray-400/60 md:max-w-[26rem]">
+              Sign in to unlock all the delicious recipes and the ability to save your favorite
+              recipes and even download them as a formatted PDF file!
+            </div>
+            <div className="flex flex-row items-center justify-center rounded-lg border border-border text-xl sm:text-2xl">
+              <UserLoginButtons extraText="Sign in" />
+            </div>
+          </div>
+        </div>
+      </Animation>
+    )
+  }
 
-	return (
-		<Animation>
-			<h1 className="sr-only">Recipe Fren - Your Recipe Fren</h1>
-			<SearchRecipes />
-			<Toaster toastOptions={TOAST_OPTIONS} />
-		</Animation>
-	)
+  return (
+    <Animation>
+      <h1>Recipe Fren - Your Recipe Fren</h1>
+      <SearchRecipes />
+      <Toaster toastOptions={TOAST_OPTIONS} />
+    </Animation>
+  )
 }
 
 export default RecipeFrenPage
